a5fc44abfcbb20bcf8a6a8760f1bfa040165d644
Jukes <jules.jouvin@gmail.com>
2023-10-14 18:56:45 +0200

    modified:   sprite_animation.py

--- sprite_animation.py
+++ sprite_animation.py
@@ -2,9 +2,10 @@ import pygame
 from settings import *
 
 class Sprite_Animation(pygame.sprite.Sprite):
-    def __init__(self, sprite_name, state, list_state, scale):
+    def __init__(self, sprite_name, state, list_state, scale, fps = 1):
         super().__init__()
         self.sprite_name = sprite_name
+        self.fps = fps
         self.image = pygame.image.load(f'assets/images/{sprite_name}/{sprite_name}_{state}/{sprite_name}_{state}0.png') # Image par défaut
         self.current_img = 0 # Frame début d'animation
         self.images = load_animation_images(sprite_name,list_state, scale) #Danger
@@ -18,12 +19,12 @@ class Sprite_Animation(pygame.sprite.Sprite):
             # Anime le sprite
             if self.current_img < len(images):
                 # Change d'image
-                self.image = images[self.current_img]
+                self.image = images[int(self.current_img)]
             else:
                 # Remise début d'animation
                 self.current_img = 0
             # Passe au sprite suivant
-            self.current_img += 1
+            self.current_img += self.fps
         else: 
             self.image = images[0]
         print(self.image)
