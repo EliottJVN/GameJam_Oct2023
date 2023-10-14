8bd58b27f55c7a0d00bac5042f7450f57818d067
Jukes <jules.jouvin@gmail.com>
2023-10-14 15:30:08 +0200

    modified:   level.py 	modified:   level.py 	modified:   menu.py

--- level.py
+++ level.py
@@ -8,8 +8,12 @@ class Level:
     def __init__(self):
         
         self.screen = pygame.display.get_surface()  
+        self.image = pygame.image.load("assets/player_left/player_left2.png")
+        self.image = pygame.transform.scale_by(self.image, 5)
+        self.rect = self.image.get_rect()
 
 
     def run(self):
 
-        self.screen.fill("black")
+        self.screen.fill("white")
+        self.screen.blit(self.image, self.rect)
