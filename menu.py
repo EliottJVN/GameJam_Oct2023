013d7d5cde71aa58251d27fc6fa86bf553911b87
Jukes <jules.jouvin@gmail.com>
2023-10-14 12:28:00 +0200

    modified:   menu.py 	modified:   settings.py

--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
 import pygame
+from settings import *
 
+print(pygame.font.get_fonts())
 
 class Menu:
 
@@ -12,7 +14,19 @@ class Menu:
         # groups
         self.all_sprites = pygame.sprite.Group()
 
+        # text
+        self.font = pygame.font.SysFont("comicsansms", FONT_SIZE_INTRO)
+        self.text = self.font.render("I N T R O", True, "black")
+        self.text_rect = self.text.get_rect(center = FONT_SIZE_INTRO_POS)
+
+
+
+    def intro(self):
+        
+        self.screen.blit(self.text, self.text_rect)
+
+
 
     def run(self):
 
-        pass
+        self.intro()
