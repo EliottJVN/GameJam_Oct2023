104084ebc9371b674989045d0d627a2def8d9556
Jukes <jules.jouvin@gmail.com>
2023-10-14 21:34:48 +0200

    modified:   level.py

--- level.py
+++ level.py
@@ -14,6 +14,7 @@ class Level:
 
         # attribu str "11", "12", "2"
         self.level_name = None
+        self.rain = False
 
         # objet
         self.player = Player()
@@ -30,12 +31,19 @@ class Level:
         self.all_sprite.add(self.player)
 
     
+    # setup level en fonction niveau
     def setup(self, level_name):
 
         # crée bon setup pour le niveau
         self.level_name = level_name
 
         if self.level_name == "11":
+            # fond
+            self.image = pygame.image.load("assets/images/backgrounds/rain.png")
+            self.image = pygame.transform.scale(self.image, (800, 800))        
+            self.rain = True
+
+            #player
             self.player.slide = True
 
         elif self.level_name == "12":
@@ -50,6 +58,7 @@ class Level:
 
         self.level_name = None
         self.image = None
+        self.rain = False
 
         self.player.slide = False
 
@@ -57,14 +66,25 @@ class Level:
         self.sprite_enemies.clear()
 
 
-    def run(self):
-
-        self.screen.fill("white")
-        self.all_sprite.draw(self.screen)
+    def update(self):
 
         # pour afficher SPACE pour frame perfect stop
         if self.level_name == "11":
             if self.player.slideActive and 450 < pygame.time.get_ticks() - self.player.curentTimeSlide < 700:
                 self.screen.blit(self.textStop, self.textStop_rect)
 
+
+    def run(self):
+
+        #fond
+        self.screen.blit(self.image, (0, 0))
+
+        #draw sprite
+        self.all_sprite.draw(self.screen)
+
+        #update ce qui se passe et draw au dessus player
+        self.update()
+
+        #update sprite
         self.all_sprite.update()
+        
