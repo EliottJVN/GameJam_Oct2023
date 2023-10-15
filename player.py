d84a7f8073a2eed17b4658277eec967041030743
Jukes <jules.jouvin@gmail.com>
2023-10-15 03:54:51 +0200

    modified:   player.py

--- player.py
+++ player.py
@@ -207,8 +207,10 @@ class Player(Sprite_Animation):
                         sprite.inventory["stick"] += 1
              
                 elif sprite.sprite_name == "collectable":
-                    self.afficher_pickable = True
-                    goesInLoop = True
+                    # check si playeur a objet si non  alors afficher E
+                    if not self.inventory:
+                        self.afficher_pickable = True
+                        goesInLoop = True
                     key = pygame.key.get_pressed()
                     # En fonction des clés mises à jour du vecteur position.
                     if key[pygame.K_e]:
