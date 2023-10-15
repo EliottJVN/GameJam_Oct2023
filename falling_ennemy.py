a81598398a69f3baaf12a57727d06eb0f582237c
eliott.jouvin <eliott.jouvin@2025.icam.fr>
2023-10-15 05:47:00 +0200

    modified:   falling_ennemy.py 	modified:   level.py

--- falling_ennemy.py
+++ falling_ennemy.py
@@ -9,11 +9,14 @@ class Falling_Ennemy(Sprite_Animation):
         # Création du rectangle
         self.rect = self.image.get_rect()
         self.rect.center = coord
+        self.save_coord = coord
         self.timer = pygame.time.get_ticks()
         
     def update(self):
         if pygame.time.get_ticks() - self.timer >= 3000 and self.state == 'hit':
             Sprite_Animation.animate(self, vect=pygame.math.Vector2((1,1)),key = self.state)
+            self.rect = self.image.get_rect()
+            self.rect.midbottom = self.save_coord
         else:
             Sprite_Animation.animate(self, key = 'idle')
     
