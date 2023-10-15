254c221fdf4210e5be00fb4ad6f00ec26b9d1b52
Pollukse <saubusse.p@gmail.com>
2023-10-15 06:49:46 +0200

    modified:   settings.py

--- settings.py
+++ settings.py
@@ -17,6 +17,10 @@ FONT_SIZE_STOP_POS = (400, 400)
 FONT_SIZE_NBRSTICK = 20
 FONT_SIZE_NBRSTICK_POS = (400, 440)
 
+INTRO_TEXT_1 = "Où et quand ai-je bien pu arriver...?"
+INTRO_TEXT_2 = "Cet endroit ne m'a pas l'air inhospitalié. Tant mieux, mon Pisto-portail vient juste de me lâcher !"
+INTRO_TEXT_3 = "J'ai malheureusement perdu toutes mes affaires donc le chemin sera long pour pouvoir réparer le Pisto-portail."
+
 
 # settings pos button
 BUTONS_POS = {"intro next": (700, 700), "menu jouer": (400, 500), "menu continuer": (400, 600), "menu quit": (400, 700)}
